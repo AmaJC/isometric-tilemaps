5e2719d7ed649ebeaf9d910cc05bf290c26e7a1b
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2019-02-18 16:44:57 -0800

    Can click to change tiles

--- Assets/TileClick.cs
+++ Assets/TileClick.cs
@@ -2,50 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEditor;
 
 public class TileClick : MonoBehaviour
 {
 	public Tilemap tilemap;
 	public Grid grid;
-	public float speed = 5.0f;
+	public TileBase tile_dirt;
+	public TileBase tile_ocean;
 
     // Start is called before the first frame update
     void Start()
     {
-        tilemap = GameObject.Find("Tilemap").GetComponent<Tilemap>();
-        Debug.Log("Found tile map!");
-        grid = GameObject.Find("Grid").GetComponent<Grid>();;
+    	LoadTiles();
     }
 
     // Update is called once per frame
     void Update()
     {
-    	float inputX = Input.GetAxis ("Horizontal");
-         float inputZ = Input.GetAxis ("Vertical");
-         
-         if (inputX != 0)
-             rotate ();
-         if (inputZ != 0)
-             move ();
-
-            void rotate(){
-     transform.Rotate (new Vector3 (0f, inputX * Time.deltaTime, 0f));
- }
- 
- void move(){
-     transform.position += transform.forward * inputZ * Time.deltaTime;
- }
-
 		// save the camera as public field if you using not the main camera
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		// get the collision point of the ray with the z = 0 plane
 		Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
 		Vector3Int position = grid.WorldToCell(worldPoint);
-		Debug.Log(position);
+		//Debug.Log(position);
 		TileBase hovered = tilemap.GetTile(position);
-		Debug.Log(hovered);
 		Debug.Log(hovered.name);
+
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			Vector3Int posToSet = position;
+			tilemap.SetTile(posToSet, tile_ocean);
+			Debug.Log("Setting");
+			Debug.Log(posToSet);
+		}
+		
     }
 
+    // Brings in all the tiles for use within the game
+    void LoadTiles()
+    {
+    	grid = GameObject.Find("Grid").GetComponent<Grid>();
+    	tilemap = GameObject.Find("Tilemap").GetComponent<Tilemap>();
+    	tile_dirt = AssetDatabase.LoadAssetAtPath("Assets/dirt.asset", typeof(TileBase)) as TileBase;
+    	tile_ocean = AssetDatabase.LoadAssetAtPath("Assets/ocean.asset", typeof(TileBase)) as TileBase;
+    }
     
 }
